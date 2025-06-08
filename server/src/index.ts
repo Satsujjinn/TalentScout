a8f6e78c2014179db52f4135adb0c0628ccdf4c6
Satsujjinn <leonjordaan10@gmail.com>
2025-06-08 22:56:49 +0200

    fix server setup for testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,6 +22,8 @@ mongoose
   .catch((err) => console.error(err));
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server running on ${port}`));
+if (require.main === module) {
+  app.listen(port, () => console.log(`Server running on ${port}`));
+}
 
 export default app;
